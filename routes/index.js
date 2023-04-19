3d0f229225e402bc8cab1f2fa9d8b41cfee3881c
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-04-19 17:27:22 -0600

    upload changes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const routes = require('express').Router();
 const jokes = require('../controllers/');
 
-routes.get('/', (req, res) => {
-  res.send('Ryan Alvord');
-});
-
 routes.get('/', jokes.displayJoke);
 
 module.exports = routes;
