2bdecbd52a4f6b9916c0cedb60a97ca03ae0af4e
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-04-19 17:26:26 -0600

    joke files update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const routes = require('express').Router();
+const jokes = require('../controllers/');
 
 routes.get('/', (req, res) => {
   res.send('Ryan Alvord');
 });
+
+routes.get('/', jokes.displayJoke);
 
 module.exports = routes;
