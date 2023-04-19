48f0b9ad0bd9b4f0f3120603fae32d4c58c9ad32
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-04-19 16:57:43 -0600

    Upload of controller file

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+displayName = (req, res) => {
+    const data =
+      'How did the telephone propose to his girlfriend? ...he gave her a ring.';
+    res.status(200).send(data);
+  };
+  
+  module.exports = {
+    displayJoke,
+  };
