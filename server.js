914f6883a90f3a6b574b6edb08db4d9eac808b03
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-04-22 13:07:59 -0600

    adjustment test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const mongodb = require('../db/connect');
+const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'))
