52dbd832579b65ea92bdeb184bbf47e6ad7d5b86
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-04-28 22:40:37 -0600

    updates for week 4

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const port = process.env.PORT || 3000;
 
 app
   .use((req, res, next) => {
-  // Who has access to the database via URL
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // What control they have over
-  // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-  // res.setHeader('Content-Type', 'application/json');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-})
+    // Who has access to the database via URL
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // What control they have over
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })  
   .use('/', require('./routes'))  
   .use('/', bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }));
