029e36e8bd30855f5e7895aa75b442e945a8f53b
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-04-28 22:37:17 -0600

    updates to week 4

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ var bodyParser = require('body-parser');
 const mongodb = require('./db/connect.js');
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+app
   .use((req, res, next) => {
   // Who has access to the database via URL
-  res.setHeader('Access-Control-Allow-Origin', 'https://cse341-contacts-frontend.netlify.app/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // What control they have over
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  // res.setHeader('Content-Type', 'application/json');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 })
+  .use('/', require('./routes'))  
   .use('/', bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }));
 
@@ -25,6 +26,3 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
-// app.listen(port, () => {
-//     console.log(`Running on port ${port}`)
-// })
