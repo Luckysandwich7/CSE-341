6acd1c059eeb0d2eca29de8e47a029d2eb922e28
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-05-02 20:06:56 -0600

    changes

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const mongodb = require('./db/connect.js');
 const port = process.env.PORT || 3000;
 
 app
+  .use('/', bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
   .use((req, res, next) => {
     // Who has access to the database via URL
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // What control they have over
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     // res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // What control they have over
     next();
   }) 
-  .use('/', require('./routes'))  
-  .use('/', bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: false }));
+  .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
