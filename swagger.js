ef600f39d837cd29c5c1aba6a7839c9ecfc9e9d7
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-05-03 22:40:37 -0600

    swagger update

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Contacts API'
+    description: 'Star Wars API'
   },
   host: 'ryanalvordcit341.onrender.com',
   schemes: ['https']
