e515998437c0abdd96c7e88982ac1eda499da384
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-05-09 20:35:09 -0600

    Render updates

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Star Wars API'
   },
-  host: 'localhost:3000',
-  schemes: ['http']
+  host: 'ryanalvordcit341.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
