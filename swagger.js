be44de2a605a50958efb0371e04b2ba2e76a50aa
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-05-09 20:29:45 -0600

    fixes for OAuth

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Star Wars API'
   },
-  host: 'ryanalvordcit341.onrender.com',
-  schemes: ['https']
+  host: 'localhost:3000',
+  schemes: ['http']
 };
 
 const outputFile = './swagger.json';
